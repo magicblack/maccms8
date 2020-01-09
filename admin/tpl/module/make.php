dcb6ec92a9362460c01da8badc935280cd8f2e3c
magicblack <heishou_521@163.com>
2020-01-09 21:14:39 +0800

    修复生成页面问题。

--- admin/tpl/module/make.php
+++ admin/tpl/module/make.php
@@ -395,8 +395,7 @@ elseif($method=='type')
 		$datacount=$p['datacount'];
 		$start=intval($p['start']);
 	}
-	if(!is_numeric($start)) { $start = 1;}
-	
+	if(empty($start)) { $start = 1;}
 	if($MAC['view'][$tab.'type'] !=2){
 		$url = empty($jump) ? '' : '?m=make-index-tab-'.$tab.'-jump-1';
  		showMsg('列表浏览模式非静态，不需要生成静态文件',$url);
@@ -429,7 +428,7 @@ elseif($method=='type')
 	$tpl->H = loadFile(MAC_ROOT_TEMPLATE.'/'.$tpl->T['t_tpl']);
 	
 	if(empty($pagesize)){ $pagesize = $tpl->getPageListSizeByCache($tab,'type'); }
-	if(!is_numeric($pagesize)) { $pagesize = 10; }
+	if(empty($pagesize)) { $pagesize = 10; }
 	if(empty($datacount)){
 		$sql = 'select count(*) from {pre}'.$tab.' where '.$pre.'_type IN ('. $tpl->T["childids"].')';
 		$datacount = $db->getOne($sql);
@@ -517,7 +516,7 @@ elseif($method=='topicindex')
 	
 	$tpl->H = loadFile(MAC_ROOT_TEMPLATE."/".$tab."_topicindex.html");
 	if(empty($pagesize)){ $pagesize = $tpl->getPageListSizeByCache('topic',$tab); }
-	if(!is_numeric($pagesize)) { $pagesize = 10;}
+	if(empty($pagesize)) { $pagesize = 10;}
 	if(empty($datacount)){
 		$sql = 'select count(*) from {pre}'.$tab.'_topic where t_hide=0 ';
 		$datacount = $db->getOne($sql);
@@ -593,7 +592,7 @@ elseif($method=='topic')
 		$datacount=$p['datacount'];
 		$start=intval($p['start']);
 	}
-	if(!is_numeric($start)) { $start = 1;}
+	if(empty($start)){ $start = 1;}
 	
 	$arr = explode(',',$ids);
 	$len = count($arr);
@@ -611,7 +610,7 @@ elseif($method=='topic')
 	$tpl->H = loadFile(MAC_ROOT_TEMPLATE."/".$tpl->T['t_tpl']);
 	
 	if(empty($pagesize)){ $pagesize = $tpl->getPageListSizeByCache($tab,'topic'); }
-	if(!is_numeric($pagesize)) { $pagesize = 10;}
+	if(empty($pagesize)) { $pagesize = 10;}
 	if(empty($datacount)){
 		$sql = 'select count(*) from {pre}'.$tab.' where '.$pre.'_id IN ( select r_b from {pre}'.$tab.'_relation where r_type=2 and r_a='.$id.')';
 		$datacount = $db->getOne($sql);
